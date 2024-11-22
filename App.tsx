62caff14b4256da1cfbd7599b3b4ed5de1be8277
scaleupVinay <vinayaka@scaleupapp@club>
2024-11-22 18:37:25 +0530

    navigation setup and Spalsh screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import SplashScreen from './src/screens/onboarding/SplashScreen';
+import {Provider} from 'react-redux';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {
+  createNavigationContainerRef,
+  NavigationContainer,
+} from '@react-navigation/native';
+import {RootNavigator} from './src/navigations';
+import {ToastProvider} from './src/components/CustomToast';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import reduxStore from './src/redux/store';
 
-const App = () => {
+export const mainNavigationRef = createNavigationContainerRef();
 
+function App(): React.JSX.Element {
   return (
-    <View style={styles.container}>
-      <SplashScreen />
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Provider store={reduxStore}>
+        <NavigationContainer ref={mainNavigationRef}>
+          <SafeAreaProvider>
+            <ToastProvider>
+              <RootNavigator />
+            </ToastProvider>
+          </SafeAreaProvider>
+        </NavigationContainer>
+      </Provider>
+    </GestureHandlerRootView>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+}
 
 export default App;
